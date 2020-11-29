04ab55ea92355d4c6c3be9436d58b3e3a78bb903
unknown <au.nguyen272@gmail.com>
2020-11-29 13:34:11 +0700

    buoi_2

--- src/Props/ProductRCC.js
+++ src/Props/ProductRCC.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
+
 export default class ProductRCC extends Component {
 
+    //this.props: là thuộc tính có sẵn của react class component dùng để nhận giá trị từ component cha truyền vào
+    //this.props thuộc tính không được gán lại giá trị mới
     render() {
-        let { sanPham } = this.props;
+        // this.props.sanPham = {};
+
+        let {sanPham} = this.props;
+        // let {maSP,tenSP,giaBan,hinhAnh} = this.props.sanPham;
         return (
-            <div classname="card text-left">
-                <img classname="card-img-top" src={sanPham.hinhAnh} alt />
-                <div classname="card-body">
-                    <h4 classname="card-title">{sanPham.tenSp}</h4>
-                    <p classname="card-text">Text</p>
+            <div className="card text-left">
+                <img className="card-img-top" src={sanPham.hinhAnh} alt />
+                <div className="card-body">
+                    <h4 className="card-title">{sanPham.tenSP}</h4>
+                    <p className="card-text">{this.props.sanPham.giaBan}</p>
                 </div>
             </div>
         )
+
     }
 }
